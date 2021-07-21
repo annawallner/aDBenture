2e15e2a1cce592d2bd2b080f1bfccd89b94e5a78
Nicole <nicolebu@edu.aau.at>
2021-07-21 16:02:52 +0200

    CodeMirror finally works

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import * as CodeMirror from 'codemirror';
+import 'codemirror/mode/sql/sql.js';
+import { WindowRef } from '../WindowRef';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
 
-  constructor() { }
+// standard is implements OnInit, with ngOnInit(): void{}
+export class EditorComponent implements OnInit, AfterViewInit {
+  @ViewChild('myEditor') myEditor;
 
-  ngOnInit(): void {
+  constructor(private winRef: WindowRef) {
   }
 
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const mime = 'text/x-mariadb';
+    const currentWindow = this.winRef.nativeWindow;
+    currentWindow.editor = CodeMirror.fromTextArea(this.myEditor.nativeElement, {
+      mode: mime,
+      indentWithTabs: true,
+      smartIndent: true,
+      lineNumbers: true,
+      // matchBrackets: true,
+      autofocus: true,
+      extraKeys: { 'Ctrl-Space': 'autocomplete' },
+    });
+  }
 }
